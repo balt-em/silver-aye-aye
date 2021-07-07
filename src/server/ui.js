38821cd40f2e8901dd4a3bc0097a98a7cff78bae
Elle Neubauer <elle@elleneub.com>
2021-07-07 11:15:36 -0500

    Only build/deploy necessary UI

--- src/server/ui.js
+++ src/server/ui.js
@@ -1,28 +1,37 @@
-export const onOpen = () => {
-  const menu = SpreadsheetApp.getUi()
-    .createMenu('My Sample React Project') // edit me!
-    .addItem('Sheet Editor', 'openDialog')
-    .addItem('Sheet Editor (Bootstrap)', 'openDialogBootstrap')
-    .addItem('About me', 'openAboutSidebar');
-
-  menu.addToUi();
+// eslint-disable-next-line import/prefer-default-export
+export const doGet = () => {
+  const title = 'Silver Aye Aye';
+  const fileName = 'webapp.html';
+  return HtmlService.createHtmlOutputFromFile(fileName)
+    .setTitle(title)
+    .setXFrameOptionsMode(HtmlService.XFrameOptionsMode.DEFAULT);
 };
 
-export const openDialog = () => {
-  const html = HtmlService.createHtmlOutputFromFile('dialog-demo')
-    .setWidth(600)
-    .setHeight(600);
-  SpreadsheetApp.getUi().showModalDialog(html, 'Sheet Editor');
-};
+// export const onOpen = () => {
+//   const menu = SpreadsheetApp.getUi()
+//     .createMenu('My Sample React Project') // edit me!
+//     .addItem('Sheet Editor', 'openDialog')
+//     .addItem('Sheet Editor (Bootstrap)', 'openDialogBootstrap')
+//     .addItem('About me', 'openAboutSidebar');
 
-export const openDialogBootstrap = () => {
-  const html = HtmlService.createHtmlOutputFromFile('dialog-demo-bootstrap')
-    .setWidth(600)
-    .setHeight(600);
-  SpreadsheetApp.getUi().showModalDialog(html, 'Sheet Editor (Bootstrap)');
-};
+//   menu.addToUi();
+// };
 
-export const openAboutSidebar = () => {
-  const html = HtmlService.createHtmlOutputFromFile('sidebar-about-page');
-  SpreadsheetApp.getUi().showSidebar(html);
-};
+// export const openDialog = () => {
+//   const html = HtmlService.createHtmlOutputFromFile('dialog-demo')
+//     .setWidth(600)
+//     .setHeight(600);
+//   SpreadsheetApp.getUi().showModalDialog(html, 'Sheet Editor');
+// };
+
+// export const openDialogBootstrap = () => {
+//   const html = HtmlService.createHtmlOutputFromFile('dialog-demo-bootstrap')
+//     .setWidth(600)
+//     .setHeight(600);
+//   SpreadsheetApp.getUi().showModalDialog(html, 'Sheet Editor (Bootstrap)');
+// };
+
+// export const openAboutSidebar = () => {
+//   const html = HtmlService.createHtmlOutputFromFile('sidebar-about-page');
+//   SpreadsheetApp.getUi().showSidebar(html);
+// };
