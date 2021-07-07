9f1e714c853936120fa9a1b8ea51f227ef4a78ee
Elle Neubauer <elle@elleneub.com>
2021-07-07 13:50:21 -0500

    calculate cost for each payment

--- src/server/sheets.js
+++ src/server/sheets.js
@@ -3,6 +3,15 @@
 const spreadsheetUrl =
   'https://docs.google.com/spreadsheets/d/1GWh-B_IMmvNniy2p82CKQ9X-eepwx70BG50xCM5r2bo/edit#gid=0';
 
+// https://stackoverflow.com/questions/2627473/how-to-calculate-the-number-of-days-between-two-dates
+function getDateDif(date1, date2) {
+  const oneDay = 24 * 60 * 60 * 1000; // hours*minutes*seconds*milliseconds
+  const firstDate = new Date(date1);
+  const secondDate = new Date(date2);
+
+  return Math.round(Math.abs((firstDate - secondDate) / oneDay)) + 1;
+}
+
 function getSheets(url) {
   if (url) {
     return SpreadsheetApp.openByUrl(spreadsheetUrl).getSheets();
@@ -39,8 +48,24 @@ export const onEdit = e => {
 
   console.log(paymentRateDict);
 
+  paymentData.shift();
   // for each payment in payments, figure out how many days are from start to end
-  // using the paymentID, get the rate, and multiply it by the number of days
+  paymentData.forEach(row => {
+    const [clientId, paymentId, startDate, endDate] = row;
+    const dateDif = getDateDif(startDate, endDate);
+    console.log(
+      'clientId, paymentId, startDate, endDate',
+      clientId,
+      paymentId,
+      startDate,
+      endDate
+    );
+    const cost = dateDif * paymentRateDict[paymentId];
+    console.log('cost', cost);
+    // const paymentId = row[]
+
+    // using the paymentID, get the rate, and multiply it by the number of days
+  });
 
   // make a new dictionary paymentId: total paid in that payment
   // make a new dictionary clientId: total paid on behalf of that client
